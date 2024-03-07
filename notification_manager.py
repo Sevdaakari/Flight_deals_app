69332b24587b002d2db51269a0e871e05f72945d
Sevda Akari <129549739+Sevdaakari@users.noreply.github.com>
2024-03-07 16:30:01 +0100

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -5,21 +5,21 @@ from twilio.rest import Client
 from flight_search import FlightSearch
 
 
-TWILIO_SID = "ACd4eedc9dedda5457c6a9fe34b36e6f66"
-TWILIO_TOKEN = "1c93c559314aaf47bd8113a5f1f1d736"
-TWILIO_NUMBER = "+15178169617"
+TWILIO_SID = "twilio_sid"
+TWILIO_TOKEN = "twilio_token"
+TWILIO_NUMBER = "my_twilio_number"
 
 
 class NotificationManager:
     def send_message(self, message):
         client = Client(TWILIO_SID, TWILIO_TOKEN)
         message = client.messages.create(body=message,
-        from_=TWILIO_NUMBER, to="+48516033828")
+        from_=TWILIO_NUMBER, to="my_phone_number")
         print(message.sid)
 
     def send_mail(self, client, mail, link):
-        my_mail = "sevdapycharm@gmail.com"
-        my_password = "ewhetdnlzldocpdw"
+        my_mail = "mymail@gmail.com"
+        my_password = "here_is_my_password"
         # "jsryepxxfiosbcnf"
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
